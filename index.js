f9128f381b46e878a6c36a2b99a2c2e113acec29
dengithub-dev <den.balinhawang@gmail.com>
2023-02-19 09:30:35 +0800

    fixing linting errors - trail spaces and indentation

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require('http')
 const qs = require('querystring')
 const calculator = require('./calculator')
- 
-const server = http.createServer(function(request, response) {
+
+const server = http.createServer(function (request, response) {
   console.dir(request.param)
  
-  if (request.method == 'POST') {
+  if (request.method === 'POST') {
     console.log('POST')
-    var body = ''
-    request.on('data', function(data) {
+    let body = ''
+    request.on('data', function (data) {
       body += data
     })
  
@@ -16,7 +16,7 @@ const server = http.createServer(function(request, response) {
       const post = qs.parse(body)
       const numbers = post.numbers
       const result = calculator.add(numbers)
-      response.writeHead(200, {'Content-Type': 'text/html'})
+      response.writeHead(200, { 'Content-Type': 'text/html' })
       response.end('Result: ' + result)
     })
   } else {
@@ -33,8 +33,7 @@ const server = http.createServer(function(request, response) {
     response.end(html)
   }
 })
- 
 const port = 3000
 const host = '127.0.0.1'
 server.listen(port, host)
-console.log(`Listening at http://${host}:${port}`)
+console.log( `Listening at http://${host}:${port}`)
